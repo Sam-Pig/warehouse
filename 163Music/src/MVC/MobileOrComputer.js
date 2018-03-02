fe05cbfe715a34e4e8f4c83b1925af0522b866d9
wobenng <op493953140@163.com>
2018-03-02 10:19:02 +0800

    test

--- 163Music/src/MVC/MobileOrComputer.js
+++ 163Music/src/MVC/MobileOrComputer.js
@@ -1,3 +1,3 @@
 if(/Android|iPhone|iPad/i.test(navigator.userAgent)) {
-    window.location.href = "https://127.0.0.1:8080/indexM.html";
+    window.location.href = "http://127.0.0.1:8080/indexM.html";
 }
