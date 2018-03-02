bade489d33b3348f9af158b273f9963c6b701b0f
wobenng <op493953140@163.com>
2018-03-02 10:26:12 +0800

    手机无法显示

--- 163Music/src/MVC/MobileOrComputer.js
+++ 163Music/src/MVC/MobileOrComputer.js
@@ -1,3 +1,3 @@
 if(/Android|iPhone|iPad/i.test(navigator.userAgent)) {
-    window.location.href = "http://127.0.0.1:8080/indexM.html";
+    window.location.href = "https://wobenng.github.io/codecamp/163Music/indexM.html";
 }
